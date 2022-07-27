21ae2680659f6933ded1fa230ed9bc25e64aa45c
camillycristina <camillycristinasilva10@gmail.com>
2022-07-27 12:20:37 -0300

    salva

---
+++ aula_270722/exercicio_2.php
@@ -0,0 +1,10 @@
+<!-- Faça um programa que exiba todos os múltiplos de 3 de 0 a 100. -->
+<?php
+
+for($cont = 0; $cont <=100; $cont++){
+    if($cont%3==0){
+        echo $cont . "</br>";
+    }
+}
+
+?>
---
+++ aula_270722/exercicio_3.php
@@ -0,0 +1,10 @@
+<!-- Faça um programa que exiba todos os ímpares de 500 a 10. -->
+<?php
+
+for($numero=500; $numero>=10; $numero--){
+    if($numero%2!=0){
+        echo $numero."</br>";
+    }
+}
+
+?>
