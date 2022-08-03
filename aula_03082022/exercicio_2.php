ea9321a6ebe549f89956cdfc007102a551fe1fac
camillycristina <camillycristinasilva10@gmail.com>
2022-08-03 12:22:30 -0300

    salva

---
+++ aula_03082022/exercicio_2.php
@@ -0,0 +1,12 @@
+<!--Crie um programa que faça a tabuada de 1 a 10, deixando o usuário escolher através da caixa de seleção a tabuada a ser gerada. -->
+
+<?php
+
+$n = $_POST['numero'];
+
+for($numero=1; $con<=10; $numero++){
+    $r=$valor*$numero;
+    echo"$valor X $numero = $r<br>";
+}
+
+?>
