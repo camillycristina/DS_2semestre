1b9a23e543de5580b8fc72b6271eb7adbe6de698
camillycristina <camillycristinasilva10@gmail.com>
2022-08-10 12:25:38 -0300

    salva

---
+++ aula_100822/exercicio1.php
@@ -0,0 +1,14 @@
+  <!-- Faça um programa que execute a tabuada de 3 valores. Cada valor deverá ser passado como parâmetro. O 1 valor devera ser passado com if, o segundo em um echo e o ultimo de sua escolha função devera ser criada em outro arquivo. -->
+
+  <?php
+
+function tabuada($v1){
+    for($numero=1; $numero<=10; $numero++){
+        $r=$n*$numero;
+        echo "$n * $numero = $r </br>";
+    }
+
+    return "<h2>$soma</h2>";
+
+}
+  ?>
