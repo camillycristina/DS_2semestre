dabe2a3261958ec07fb49fa316537543928dae1f
camillycristina <camillycristinasilva10@gmail.com>
2022-08-24 12:28:33 -0300

    salva

---
+++ aula_240822_Vetores/exemplo6.php
@@ -0,0 +1,19 @@
+<html>
+    <head>
+        <meta >
+</head>
+<body>
+    <div>
+        <pre>
+            <?php
+              $dados = array(
+                1,
+                2,
+                3
+              );
+              echo var_dump($dados);
+              echo "</br></br>";
+               $dados[]
+            ?>
+</body>
+</html>
