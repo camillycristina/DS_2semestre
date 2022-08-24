2850e06e63a0ce35c812bd01e968579106fa7939
camillycristina <camillycristinasilva10@gmail.com>
2022-08-24 09:05:10 -0300

    salva

--- aula_17082022/exercicio4.php
+++ aula_17082022/exercicio4.php
@@ -1,1 +1,22 @@
 <!--faça um programa para enviar dois valores para uma função atraves de duas chamadas, e mostrar todos os numeros impares e entre eles, em ordem decrecentes -->
+<?php
+
+function verificar($v1, $v2){
+    if($v1 > $v2){
+        while($v1 >= $v2){
+            if($v1 % 2==1){
+                echo $v1 . "</br>";
+            }
+            $v1--;
+        }
+    }
+    else{
+        while($v2 >= $v1){
+            if($v2 % 2 == 1){
+                echo $v2 . "</br>";
+            }
+            $v2--;
+        }
+    }
+}
+echo verificar(10, 52);
